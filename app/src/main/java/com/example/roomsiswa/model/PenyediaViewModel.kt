df04e213263dd4f597ccaced02096d5cdffed4cf
DhiyaulhaqKhairunnisa00 <dhiyacacaa00@gmail.com>
2023-12-04 21:06:29 +0700

    add file penyedia view model beserta isi nya

--- app/src/main/java/com/example/roomsiswa/model/PenyediaViewModel.kt
+++ app/src/main/java/com/example/roomsiswa/model/PenyediaViewModel.kt
@@ -1,2 +1,27 @@
 package com.example.roomsiswa.model
 
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.roomsiswa.AplikasiSiswa
+
+object PenyediaViewModel{
+    val Factory = viewModelFactory {
+
+        initializer {
+            HomeViewModel(aplikasiSiswa().container.repositorySiswa)
+        }
+
+        initializer {
+            EntryViewModel(aplikasiSiswa().container.repositorySiswa)
+        }
+    }
+}
+
+/**
+ * Fungsi ekstensi query untuk objek [Application] dan mengembalikan sebuah instance dari
+ * [AplikasiSiswa].
+ */
+fun CreationExtras.aplikasiSiswa():AplikasiSiswa =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa)
