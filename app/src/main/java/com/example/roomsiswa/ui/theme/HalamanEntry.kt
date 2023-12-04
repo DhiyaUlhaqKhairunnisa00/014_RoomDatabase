5da0627eb57fad2320027da1ef2e983f0b135c02
DhiyaulhaqKhairunnisa00 <dhiyacacaa00@gmail.com>
2023-12-04 21:27:53 +0700

    tambah outlined text detail siswa nama

--- app/src/main/java/com/example/roomsiswa/ui/theme/HalamanEntry.kt
+++ app/src/main/java/com/example/roomsiswa/ui/theme/HalamanEntry.kt
@@ -2,10 +2,14 @@ package com.example.roomsiswa.ui.theme
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
 import com.example.roomsiswa.R
 import com.example.roomsiswa.model.DetailSiswa
 import com.example.roomsiswa.navigasi.DestinasiNavigasi
@@ -33,6 +37,13 @@ fun FormInputSiswa(
         modifier = modifier,
         verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_medium))
     ){
-        
+        OutlinedTextField(
+            value = detailSiswa.nama,
+            onValueChange = {onValueChange(detailSiswa.copy(nama = it))},
+            label = { Text(stringResource(R.string.nama))},
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
     }
 }
