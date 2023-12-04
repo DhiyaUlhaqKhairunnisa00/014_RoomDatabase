7350e85fd5a069333010ba3bc8656d591b3fac75
DhiyaulhaqKhairunnisa00 <dhiyacacaa00@gmail.com>
2023-12-04 21:21:32 +0700

    add fun form input siswa bagian tengah kebawah dari halaman entry

--- app/src/main/java/com/example/roomsiswa/ui/theme/HalamanEntry.kt
+++ app/src/main/java/com/example/roomsiswa/ui/theme/HalamanEntry.kt
@@ -2,7 +2,9 @@ package com.example.roomsiswa.ui.theme
 
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import com.example.roomsiswa.R
+import com.example.roomsiswa.model.DetailSiswa
 import com.example.roomsiswa.navigasi.DestinasiNavigasi
 
 object DestinasiEntry: DestinasiNavigasi {
@@ -13,3 +15,14 @@ object DestinasiEntry: DestinasiNavigasi {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun EntrySiswaScreen(){}
+
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormInputSiswa(
+    detailSiswa: DetailSiswa,
+    modifier: Modifier = Modifier,
+    onValueChange: (DetailSiswa) -> Unit = {},
+    enabled: Boolean = true
+){}
