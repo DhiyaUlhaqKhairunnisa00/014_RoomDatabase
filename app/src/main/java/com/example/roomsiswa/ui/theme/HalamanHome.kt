10e1d677728f504959081beab05b2ada757105d5
DhiyaulhaqKhairunnisa00 <dhiyacacaa00@gmail.com>
2023-12-05 11:00:43 +0700

    tambah fun body home dengan column

--- app/src/main/java/com/example/roomsiswa/ui/theme/HalamanHome.kt
+++ app/src/main/java/com/example/roomsiswa/ui/theme/HalamanHome.kt
@@ -16,6 +16,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.unit.dp
@@ -26,6 +27,18 @@ import com.example.roomsiswa.navigasi.DestinasiNavigasi
 object DestinasiHome : DestinasiNavigasi{
     override val route = "home"
     override val titleRes = R.string.app_name
+}
+
+@Composable
+fun BodyHome(
+    itemSiswa: List<Siswa>,
+    modifier: Modifier=Modifier){
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+    ) {
+
+    }
 }
 
 @Composable
