fc2bc7df1e9de63a3128de3587e87c773d2d5759
DhiyaulhaqKhairunnisa00 <dhiyacacaa00@gmail.com>
2023-12-05 11:04:26 +0700

    add if else di fun body home

--- app/src/main/java/com/example/roomsiswa/ui/theme/HalamanHome.kt
+++ app/src/main/java/com/example/roomsiswa/ui/theme/HalamanHome.kt
@@ -19,6 +19,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.example.roomsiswa.R
 import com.example.roomsiswa.data.Siswa
@@ -37,7 +39,18 @@ fun BodyHome(
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = modifier
     ) {
-
+        if (itemSiswa.isEmpty()){
+           Text(
+               text = stringResource(R.string.deskripsi_no_item),
+               textAlign = TextAlign.Center,
+               style = MaterialTheme.typography.titleLarge
+               ) 
+        } else {
+            ListSiswa(
+                itemSiswa = itemSiswa,
+                modifier = Modifier.padding(horizontal = dimensionResource(id = R.dimen.padding_small))
+                )
+        }
     }
 }
 
